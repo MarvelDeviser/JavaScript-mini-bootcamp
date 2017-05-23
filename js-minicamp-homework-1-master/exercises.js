9cc756ec8732c7a1588f3d7f549dba0223e01e0a
mixelpixel <mixelpix@mac.com>
2017-05-23 15:19:06 -0400

    Homework 1

--- js-minicamp-homework-1-master/exercises.js
+++ js-minicamp-homework-1-master/exercises.js
@@ -1,99 +1,140 @@
 //Do not change any of the function names
 
 function multiplyByTen(num) {
-  //return num after multiplying it by ten
-  //code here
+  /* return num after multiplying it by ten */
+  // var    product = num * 10;
+  // return product;
+  return num * 10;
 }
 
 function subtractFive(num) {
-  //return num after subtracting five
-  //code here
+  /* return num after subtracting five */
+  var    sub = num - 5;
+  return sub;
 }
 
 function areSameLength(str1, str2) {
-  //return true if the two strings have the same length
-  //otherwise return false
-  //code here
+  /* return true if the two strings have the same length */
+  /* otherwise return false */
+  if (str1.length == str2.length) {
+    return true;
+  }
+  return false;
 }
 
 function areEqual(x, y) {
-  //return true if x and y are the same
-  //otherwise return false
-  //code here
+  /* return true if x and y are the same */
+  /* otherwise return false */
+  if (x == y) {
+    return true;
+  }
+  return false;
 }
 
 function lessThanNinety(num) {
-  //return true if num is less than ninety
-  //otherwise return false
-  //code here
+  /* return true if num is less than ninety */
+  /* otherwise return false */
+  if (num < 90) {
+    return true;
+  }
+  return false;
 }
 
 function greaterThanFifty(num) {
-  //return true if num is greater than fifty
-  //otherwise return false
-  //code here
+  /* return true if num is greater than fifty */
+  /* otherwise return false */
+  if (num > 50) {
+    return true;
+  }
+  return false;
 }
 
 function add(x, y) {
-  //add x and y together and return the value
-  //code here
+  /* add x and y together and return the value */
+  // var    sum = x + y;
+  // return sum;
+  return x + y;
 }
 
 function subtract(x, y) {
-  //subtract y from x and return the value
-  //code here
+  /* subtract y from x and return the value */
+  // var    difference = x - y;
+  // return difference;
+  return x - y;
 }
 
 function divide(x, y) {
-  //divide x by y and return the value
-  //code here
+  /* divide x by y and return the value */
+  // var    quotient = x / y;
+  // return quotient;
+  return x / y;
 }
 
 function multiply(x, y) {
-  //multiply x by y and return the value
-  //code here
+  /* multiply x by y and return the value */
+  // var    product = x * y;
+  // return product;
+  return x * y;
 }
 
 function getRemainder(x, y) {
-  //return the remainder from dividing x by y
-  //code here
+  /* return the remainder from dividing x by y */
+  // var    remainder = x % y;
+  // return remainder;
+  return x % y;
 }
 
 function isEven(num) {
-  //return true if num is even
-  //otherwise return false
-  //code here
+  /* return true if num is even */
+  /* otherwise return false */
+  if (num % 2 == 0) {
+    return true;
+  }
+  return false;
 }
 
 function isOdd(num) {
-  //return true if num is false
-  //otherwise return false
-  //code here
+  /* return true if num is false */
+  /* otherwise return false */
+  if (num % 2 == 0) {
+    return false;
+  }
+  return true;
 }
 
 function square(num) {
-  //square num and return the new value
-  //code here
+  /* square num and return the new value */
+  // var    squared = Math.pow(num, 2);
+  // return squared;
+  return Math.pow(num, 2);
 }
 
 function cube(num) {
-  //cube num and return the new value
-  //code here
+  /* cube num and return the new value */
+  // var    cubed = Math.pow(num, 3);
+  // return cubed;
+  return Math.pow(num, 3);
 }
 
 function raiseToPower(num, exponent) {
-  //raise num to whatever power is passed in as exponent
-  //code here
+  /* raise num to whatever power is passed in as exponent */
+  // var    exponential = Math.pow(num, exponent);
+  // return exponential;
+  return Math.pow(num, exponent);
 }
 
 function roundNumber(num) {
-  //round num and return it
-  //code here
+  /* round num and return it */
+  // var    round = Math.round(num);
+  // return round;
+  return Math.round(num);
 }
 
 function roundUp(num) {
-  //round num up and return it
-  //code here
+  /* round num up and return it */
+  // var    ceiling = Math.ceil(num);
+  // return ceiling;
+  return Math.ceil(num);
 }
 
 function addExclamationPoint(str) {
@@ -115,7 +156,7 @@ function getGreeting(name) {
 }
 
 //If you can't remember these area formulas then head over to Google or look at the test code.
- 
+
 function getRectangleArea(length, width) {
   //return the area of the rectangle by using length and width
   //code here
